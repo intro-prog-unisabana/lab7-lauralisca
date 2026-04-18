9ef589e8ee9e900605788c4d0fa56ca1c7fd20a1
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-18 16:13:33 -0500

    lab 7

--- main.py
+++ main.py
@@ -8,6 +8,28 @@ def main() -> None:
         option = input()
         if option == "1":
             print("Enter the website and the new password:")
+            data = input().split()
+            if len(data)< 2:
+                print("Input is in the wrong format!")
+                continue
+            website, password = data
+            if len(password) <12 :
+                print("Password is too short!")
+                continue
+            result = change_password(filename, website, password)
+            if not result:
+                print("Website not found! Operation failed.")
+            else:
+                print("Password changed.")
+        elif option == "2":
+            print("Enter the website, username, and password:")
+            data = input().split()
+            if len(data) < 3:
+                print("Input is in the wrong format!")
+                continue
+            
+
+
 
 
 
