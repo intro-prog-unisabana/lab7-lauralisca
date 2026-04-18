74ca90c349147fc83ba7514b6aff98c590c3eaa3
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-18 16:01:46 -0500

    lab 7

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from password_manager import add_login, change_password, encrypt_passwords_in_file
 
-
 def main() -> None:
-    """TODO: Parte 5 - programa principal interactivo."""
-    pass
+    filename = input("Enter the CSV file name:\n")
+    encrypt_passwords_in_file(filename)
+    while True:
+        print("Options: (1) Change Password, (2) Add Password, (3) Quit:")
+        option = input()
+        if option == "1":
+            print("Enter the website and the new password:")
+
 
 
 if __name__ == "__main__":
